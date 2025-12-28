3191af9cc472a29c54c0d4b17e72d037b680d081
Anthony Guzzardo <anthonyguzzardo@Anthonys-MacBook-Pro-2.local>
2025-12-28 16:10:38 -0600

    added page navigation

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cargo Land
+**Terrains are flooding everywhere and CargoLand Corporation with their new government contracts needs to fly in their equipment to pump out the water**
---
+++ public/index.js
@@ -0,0 +1,16 @@
+// STATE
+
+const startButton = document.getElementById("start-button");
+
+// HTML
+
+// EVENT LISTENERS
+startButton.addEventListener('click', e =>{
+    e.preventDefault();
+    window.location.href = 'game.html';
+})
+// FUNCTIONS
+
+function startGame(){
+    document.prvent
+}
